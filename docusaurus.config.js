4b588a98b805eac13d5392a9cae90a2d7c691f4e
Sebastian Mellen <s@slm.space>
2021-11-08 14:30:04 -0500

    remove homepage fluff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,38 +1,38 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Cerebrum',
-  tagline: 'Docs',
-  url: 'https://cerebrum-docs.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'cerebruminc', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "Cerebrum",
+  tagline: "Docs",
+  url: "https://docs.cerebrum.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "cerebruminc", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/cerebruminc/docs/edit/main/website/',
+          editUrl: "https://github.com/cerebruminc/docs/edit/main/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/cerebruminc/docs/edit/main/website/blog/',
+            "https://github.com/cerebruminc/docs/edit/main/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -42,40 +42,40 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Cerebrum Docs',
+        title: "Cerebrum Docs",
         logo: {
-          alt: 'Cerebrum Logo',
-          src: 'img/logo.svg',
+          alt: "Cerebrum Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "API Documentation",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // { to: "/blog", label: "Blog", position: "left" },
           {
-            href: 'https://github.com/cerebruminc/docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/cerebruminc/docs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               // {
               //   label: 'Stack Overflow',
@@ -92,15 +92,15 @@ const config = {
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
+              // {
+              //   label: "Blog",
+              //   to: "/blog",
+              // },
               {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/cerebruminc/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/cerebruminc/docs",
               },
             ],
           },
